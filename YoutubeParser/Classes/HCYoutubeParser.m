b4080e7b1c17bae2efab0b8034e25332e97a5747
thiagoperes <thiago@appkraft.net>
2014-02-17 14:35:38 -0300

    Added support for "youtbe.googleapis.com" URLs

--- YoutubeParser/Classes/HCYoutubeParser.m
+++ YoutubeParser/Classes/HCYoutubeParser.m
@@ -89,11 +89,16 @@
 
 + (NSString *)youtubeIDFromYoutubeURL:(NSURL *)youtubeURL {
     NSString *youtubeID = nil;
+    
+    NSLog(@"%@\n%@\n%@", youtubeURL.host, [youtubeURL dictionaryForQueryString], [youtubeURL pathComponents]);
+    
     if ([youtubeURL.host isEqualToString:@"youtu.be"]) {
         youtubeID = [[youtubeURL pathComponents] objectAtIndex:1];
     } else if([youtubeURL.absoluteString rangeOfString:@"www.youtube.com/embed"].location != NSNotFound){
         youtubeID = [[youtubeURL pathComponents] objectAtIndex:2];
-    }else {
+    } else if([youtubeURL.host isEqualToString:@"youtube.googleapis.com"]) {
+        youtubeID = [[youtubeURL pathComponents] objectAtIndex:2];
+    } else {
         youtubeID = [[[youtubeURL dictionaryForQueryString] objectForKey:@"v"] objectAtIndex:0];
     }
     return youtubeID;
